3925441b6baa4fb12d915cfe6b8e75a0733034e7
agam kumar <agam kumar>
2017-05-28 11:42:16 +0530

    api update

--- application/controllers/Apis.php
+++ application/controllers/Apis.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Apis extends Note_controller {
+class Apis extends CI_Controller {
 
 	function __construct(){
 		parent:: __construct();
